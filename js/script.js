3ff748ae0161c1183dcd0ac8b1850d7988a035d1
PaulValle <paul.valle96@gmail.com>
2018-09-20 13:47:04 -0500

    Funciones para listar los trabajos en cola de Render, listar trabajos finalizados, todos con botones de guardar, pausar, reanudar, eliminar.

---
+++ js/script.js
@@ -0,0 +1,381 @@
+/**
+ * ownCloud - blendmaxmayapluginGUI
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Kimberly Muñoz <kipamuno@espol.edu.ec>
+ * @author Jean Paul Valle <jpvalle@espol.edu.ec>
+ * @copyright Edcom-Espol 2018
+ */
+
+(function ($, OC) {
+	var timer = false;
+	
+	$(document).ready(function () {
+		ajaxRequestDon();
+		$( window ).resize(function() {
+			if($(window).width() <= 770) {
+				$( ".jobnav-top" ).show();
+				$("#job-nav").height("45px");
+			} else {
+				$( "#job-nav ul" ).show();
+				$( ".jobnav-top" ).hide();
+				$("#job-nav").height("100%");
+			}
+		});
+		$(document).on('click', '.folderPath', function (){
+			$('#loading-state').show();
+			var url = OC.generateUrl('/apps/blendmaxmayaplugingui/cp');
+			var data = {
+				folder: $(this).attr("nameFolder"),
+				id_job: $(this).attr("id_job"),
+				host_name :$(this).attr("host_name"),
+			};
+
+			$.post(url, data).success(function (response) {
+				$('#loading-state').hide();
+				window.location.href = 'owncloud/apps/files?dir=//Documents';
+			});
+		});
+        $(document).on('click', '.borrarRenderFinalizado', function (){
+			$('#loading-state').show();
+			var url = OC.generateUrl('/apps/blendmaxmayaplugingui/dltjob');
+			var data = {
+                id_job: $(this).attr("id_job"),
+				host_name :$(this).attr("host_name"),
+			};
+
+			$.post(url, data).success(function (response) {
+				$('#loading-state').hide();
+				location.reload();
+			});
+		});
+         $(document).on('click', '.pausarRender', function (){
+			//$('#loading-state').show();
+			var url = OC.generateUrl('/apps/blendmaxmayaplugingui/pausejob');
+			var data = {
+                id_job: $(this).attr("id_job"),
+				host_name :$(this).attr("host_name"),
+			};
+
+			$.post(url, data).success(function (response) {
+				//$('#loading-state').hide();
+				//$('#processJob').click();
+			});
+		});
+        $(document).on('click', '.reanudarRender', function (){
+			//$('#loading-state').show();
+			var url = OC.generateUrl('/apps/blendmaxmayaplugingui/startjob');
+			var data = {
+                id_job: $(this).attr("id_job"),
+				host_name :$(this).attr("host_name"),
+			};
+
+			$.post(url, data).success(function (response) {
+				//$('#loading-state').hide();
+				//$('#processJob').click();
+			});
+		});
+
+		$('#endjobmessage').hide();
+		$('#processjobmessage').hide();
+
+        $('#endJob').attr("class", "selectedOption");
+
+        if(localStorage.getItem('id') == '#endJob') {
+        	localStorage.setItem('id', '');
+			showEndJob();
+		} else if(localStorage.getItem('id') == '#processJob') {
+			localStorage.setItem('id', '');
+			showProcessJob();
+		}
+
+		$('#endJob').click(function () {
+			$('#endJob').attr("class", "selectedOption");
+			$('#processJob').attr("class", " ");
+			if (timer !== false){
+				clearInterval(timer);
+				timer = false;
+				ajaxRequestDon();
+			}
+		});
+
+		$('#processJob').click(function () {
+			$('#bigCont').empty();
+			$('#processJob').attr("class", "selectedOption");
+			$('#endJob').attr("class", " ");
+            if (timer !== false){
+				clearInterval(timer);
+			}
+			else
+			{
+				timer = setInterval(ajaxRequestProcess, 1000);
+			}
+		});
+		$('#closeNav').click(function() {
+			document.getElementById("job-nav").style.height = "45px";
+			$('#openNav').css('display', 'block');
+			$('#job-nav ul').css('display', 'none');
+			$(this).hide();
+		});
+		$('#openNav').click(function() {
+			document.getElementById("job-nav").style.height = "auto";
+			$('#closeNav').css('display', 'block');
+			$('#job-nav ul').css('display', 'block');
+			$(this).hide();
+		});
+
+
+		$("#search").on("keyup", function() {
+			var value = $(this).val();
+
+			$("table tr").each(function(index) {
+				if (index !== 0) {
+
+					$row = $(this);
+
+					var id = $row.find("#nami").text();
+
+				if (id.indexOf(value) !== 0) {
+					$row.hide();
+				}
+				else {
+					$row.show();
+				}
+				}
+			});
+		});
+	});
+
+	function showEndJob() {
+		 $('#endJob').attr("class", "selectedOption");
+		 $('#processJob').attr("class", " ");
+		 $('#endjobmessage').show();
+		 $('#processjobmessage').hide();
+		 $('#bigCont').show();
+		$('#endJob').attr("class", "selectedOption");
+			$('#processJob').attr("class", " ");
+			if (timer !== false){
+				clearInterval(timer);
+				timer = false;
+				ajaxRequestDon();
+			}
+	}
+
+	function showProcessJob() {
+		 $('#search').hide();
+		 $('#bigCont').hide();
+		 $('#processjobmessage').show();
+		 $('#processJob').attr("class", "selectedOption");
+		 $('#endJob').attr("class", " ");
+		$('#bigCont').empty();
+			$('#processJob').attr("class", "selectedOption");
+			$('#endJob').attr("class", " ");
+            if (timer !== false){
+				clearInterval(timer);
+			}
+			else
+			{
+				timer = setInterval(ajaxRequestProcess, 1000);
+			}
+	}
+
+	function ajaxRequestDon(){
+
+ 		$('#processjobmessage').hide();
+		var jobsArray = { "data" :[]};
+		var url = OC.generateUrl('/apps/blendmaxmayaplugingui/echo');
+			var data = {
+				type: "jobs"
+			};
+			$.post(url, data).success(function (response) {
+				$('#bigCont').empty();
+				var listnum=0;
+				var completeData = JSON.parse(response.get);
+				var count = Object.keys(completeData.jobs).length;
+              
+				for (var i = 0; i < count; i++) {
+                        
+					    if (completeData.jobs[i].state === " DON" && completeData.jobs[i].user_name === $('#userNameFront').text().toLowerCase()){
+					    	listnum+=1;
+					    	/*Formato para presentar fecha*/
+							var utcSeconds = completeData.jobs[i].time_started;
+							var d = new Date(0);
+							d.setUTCSeconds(utcSeconds);
+							var date = formatDate(d);
+
+							/*Formato para presentar hora inicio*/
+
+							var utcSecondsInicio = completeData.jobs[i].time_started;
+							var d_Inicio = new Date(0);
+							d_Inicio.setUTCSeconds(utcSecondsInicio);
+							var horaInicio = formatHour(d_Inicio);
+
+
+							/*Formato para presentar hora fin*/
+							var utcSecondsFin = completeData.jobs[i].time_done;
+							var d_Fin = new Date(0);
+							d_Fin.setUTCSeconds(utcSecondsFin);
+							var horaFin = formatHour(d_Fin);
+                            //alert(horaInicio);
+                            //alert(horaFin);
+                            //var c = ((parseInt(horaFin)-parseInt(horaInicio))/1000);
+                            //alert(parseInt(c)); // resultado 5;
+							var hora1 = horaFin.split(":"),
+                            hora2 = horaInicio.split(":"),
+                            t1 = new Date(),
+                            t2 = new Date();
+
+                            t1.setHours(hora1[0], hora1[1], hora1[2]);
+                            t2.setHours(hora2[0], hora2[1], hora2[2]);
+
+                            //Aquí hago la resta
+                            t1.setHours(t1.getHours() - t2.getHours(), t1.getMinutes() - t2.getMinutes(), t1.getSeconds() - t2.getSeconds());
+
+                            //Imprimo el resultado
+                            //alert("La diferencia es de: " + (t1.getHours() ? t1.getHours() + (t1.getHours() > 1 ? " horas" : " hora") : "") + (t1.getMinutes() ? ", " + t1.getMinutes() + (t1.getMinutes() > 1 ? " minutos" : " minuto") : "") + (t1.getSeconds() ? (t1.getHours() || t1.getMinutes() ? " y " : "") + t1.getSeconds() + (t1.getSeconds() > 1 ? " segundos" : " segundo") : ""));
+                            var diferencia=""+(t1.getHours() ? t1.getHours() + (t1.getHours() > 1 ? " horas" : " hora") : "") + (t1.getMinutes() ? ", " + t1.getMinutes() + (t1.getMinutes() > 1 ? " minutos" : " minuto") : "") + (t1.getSeconds() ? (t1.getHours() || t1.getMinutes() ? " y " : "") + t1.getSeconds() + (t1.getSeconds() > 1 ? " segundos" : " segundo") : "");
+                            
+                            
+                            var percentage = "<div class='progress-bar' role='progressbar' aria-valuenow= '60' aria-valuemin='0' aria-valuemax='100' style='width:100%;'>"+ completeData.jobs[i].blocks[0].p_percentage+"%</div>";
+							jobsArray.data.push({"id_job":completeData.jobs[i].id, "host_name": completeData.jobs[i].host_name, "listnum":listnum, "name":completeData.jobs[i].name, "date": date, "horaInicio": horaInicio, "horaFin": horaFin, "percentage":percentage,"diferencia":diferencia});
+					}
+				}
+				if(listnum >= 1){
+					$('#endjobmessage').hide();
+					$('#search').show();
+					addData(jobsArray.data.sort(comp));
+					$('#job-progress').show();
+				} else {
+					$('#endjobmessage').show();
+					$('#search').hide();
+					$('#job-progress').hide();
+				}
+			});
+		};
+
+	function ajaxRequestProcess(){
+
+        $('#endjobmessage').hide();
+		var jobsArray = [];
+		var url = OC.generateUrl('/apps/blendmaxmayaplugingui/echo');
+		var data = {
+			type: "jobs"
+		};
+		$.post(url, data).done(function (response) {
+			$('#bigCont').empty();
+			var listnum=0;
+			var completeData = JSON.parse(response.get);
+			var count = Object.keys(completeData.jobs).length;
+			
+			for (var i = 0; i < count; i++) {
+                //alert("s:"+completeData.jobs[i].blocks[0].name);
+                //alert("estado 1:"+completeData.jobs[i].state);
+                //alert("user 1:"+completeData.jobs[i].user_name + " , "+$('#userNameFront').text().toLowerCase());
+                var logo;
+                switch(completeData.jobs[i].blocks[0].name){
+                    case "blenderRender":
+                        logo="Blenderlogo.svg";
+                        break;
+                    case "max":
+                        logo="maya.png";
+                        break;
+                    case "maya":
+                        logo="3dmax.png";
+                        break;
+                }
+				if ((completeData.jobs[i].state === " RDY RUN" && completeData.jobs[i].user_name === $('#userNameFront').text().toLowerCase()) || (completeData.jobs[i].state === " RDY" && completeData.jobs[i].user_name === $('#userNameFront').text().toLowerCase())){
+					
+                    listnum+=1;
+
+					var utcSeconds = completeData.jobs[i].time_started;
+					var d = new Date(0);
+					d.setUTCSeconds(utcSeconds);
+					var date = formatDate(d);
+                    
+					if (completeData.jobs[i].blocks[0].p_percentage != null) {
+                        //alert(completeData.jobs[i].id);
+						jobsArray.push('<div class="row panel panel-default"><div class=" progress-icon"><img class="icon" src="/owncloud/apps/blendmaxmayaplugingui/img/'+logo+'"></img></div><div class=" progress-card-body panel-body"><div class="panel body-head"><h4>'+completeData.jobs[i].name+'</h4><a id_job="'+completeData.jobs[i].id+'" host_name="'+ completeData.jobs[i].host_name +'" href="#" class="pausarRender"><img width="35px" src="/owncloud/apps/blendmaxmayaplugingui/img/pause.png"></img></a><a id_job="'+completeData.jobs[i].id+'" host_name="'+ completeData.jobs[i].host_name +'" href="#" class="borrarRenderFinalizado"><img width="25px" src="/owncloud/apps/blendmaxmayaplugingui/img/delete.png"></a></div><div class="panel body-info"><div class="panel row-info"><p class="info-label">Fecha: </p><p class="info-text">'+date+'</p></div><div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: '+completeData.jobs[i].blocks[0].p_percentage+'%;">'+completeData.jobs[i].blocks[0].p_percentage+'%</div></div></div></div></div>');
+					}
+					else {
+                        
+						jobsArray.push('<div class="row panel panel-default"><div class=" progress-icon"><img class="icon" src="/owncloud/apps/blendmaxmayaplugingui/img/'+logo+'"></img></div><div class=" progress-card-body panel-body"><div class="panel body-head"><h4>'+completeData.jobs[i].name+'</h4><a id_job="'+completeData.jobs[i].id+'" host_name="'+ completeData.jobs[i].host_name +'" href="#" class="pausarRender"><img width="35px" src="/owncloud/apps/blendmaxmayaplugingui/img/pause.png"></img></a><a id_job="'+completeData.jobs[i].id+'" host_name="'+ completeData.jobs[i].host_name +'" href="#" class="borrarRenderFinalizado"><img width="25px" src="/owncloud/apps/blendmaxmayaplugingui/img/delete.png"></a></div><div class="panel body-info"><div class="panel row-info"><p class="info-label">Fecha: </p><p class="info-text">'+date+'</p></div><div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 1%;"> 1% </div></div></div></div></div>');
+
+					}
+				}else if((completeData.jobs[i].state === " RDY OFF" && completeData.jobs[i].user_name === $('#userNameFront').text().toLowerCase())){
+                    //alert("hola");
+                    listnum+=1;
+                    var utcSeconds = completeData.jobs[i].time_started;
+					var d = new Date(0);
+					d.setUTCSeconds(utcSeconds);
+					var date = formatDate(d);
+                        jobsArray.push('<div class="row panel panel-default"><div class=" progress-icon"><img class="icon" src="/owncloud/apps/blendmaxmayaplugingui/img/'+logo+'"></img></div><div class=" progress-card-body panel-body"><div class="panel body-head"><h4>'+completeData.jobs[i].name+'</h4><a id_job="'+completeData.jobs[i].id+'" host_name="'+ completeData.jobs[i].host_name +'" href="#" class="reanudarRender"><img width="35px" src="/owncloud/apps/blendmaxmayaplugingui/img/play.png"></img></a><a id_job="'+completeData.jobs[i].id+'" host_name="'+ completeData.jobs[i].host_name +'" href="#" class="borrarRenderFinalizado"><img width="25px" src="/owncloud/apps/blendmaxmayaplugingui/img/delete.png"></a></div><div class="panel body-info"><div class="panel row-info"><p class="info-label">Fecha: </p><p class="info-text">'+date+'</p></div><div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: '+completeData.jobs[i].blocks[0].p_percentage+'%;">'+completeData.jobs[i].blocks[0].p_percentage+'%</div></div></div></div></div>');
+                }
+			}
+			//jobsArray.push('<button onclick="btnPause()">adasdasd</button>');
+			if(listnum >= 1){
+				$('#processjobmessage').hide();
+				$('#search').show();
+				$('#bigCont').html(jobsArray);
+				$('#bigCont').show();
+				$('#job-progress').show();
+			}
+			else {
+				$('#processjobmessage').show();
+				$('#search').hide();
+				$('#job-progress').hide();
+			}
+		});
+	};
+	
+	function btnPause(){
+		alert("Pausado");
+	};
+    function btnEliminar(){
+		alert("ELiminando");
+	};
+
+	function formatDate(d){
+		date = new Date(d)
+		var dd = date.getDate();
+		var mm = date.getMonth()+1;
+		var yyyy = date.getFullYear();
+		if(dd<10){dd='0'+dd}
+		if(mm<10){mm='0'+mm};
+		return d = dd+'/'+mm+'/'+yyyy
+	};
+
+	function addZero(i) {
+	    if (i < 10) {
+	        i = "0" + i;
+	    }
+    	return i;
+	}
+
+	function formatHour(d) {
+	    var hour = new Date(d);
+	    var h = addZero(hour.getHours());
+	    var m = addZero(hour.getMinutes());
+	    var s = addZero(hour.getSeconds());
+	    return hour = h + ":" + m + ":" + s;
+	}
+
+	function addData(jobsArray){
+		var cont = 0;
+		// $('#bigCont').append('<table class="table"><thead><tr><th>#</th><th>Nombre de Escena</th><th>Fecha</th><th>Hora Inicio</th><th>Hora Fin</th><th>Porcentaje de Progreso</th><th>Estado</th><th>Ver Archivos</th></tr></thead><tbody id="tbodyid"></tbody></table>');
+		$('#bigCont').append('<table class="table"><thead><tr><th>#</th><th>Nombre de Escena</th><th>Fecha</th><th>Tiempo de renderizado</th><th>Descargar</th><th>Borrar</th></tr></thead><tbody id="tbodyid"></tbody></table>');
+		$.each(jobsArray, function (index, value) {
+			cont+=1;
+			$(".table tbody").append("<tr><td>"+ cont +"</td><td id='nami'>"+ value.name +" </td><td> "+ value.date +" </td><td>"+ value.diferencia+"</td><td><a host_name="+ value.host_name +" id_job=" + value.id_job +" nameFolder='"+value.name+"' href='#' class='folderPath'><i class='fa fa-floppy-o fa-2x'></i></a></td><td><a host_name="+ value.host_name +" id_job=" + value.id_job +" nameFolder='"+value.name+"' href='#' class='borrarRenderFinalizado'><img width='30px' src='/owncloud/apps/blendmaxmayaplugingui/img/delete.png'></a></td></tr>");
+		});
+		$('.table').paging({
+			limit:5
+		});
+	};
+
+	function comp(a, b) {
+    return parseFloat(b.listnum) - parseFloat(a.listnum);
+};
+
+})(jQuery, OC);
