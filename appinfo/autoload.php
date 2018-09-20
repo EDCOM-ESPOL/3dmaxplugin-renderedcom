36f91be909b6a7ae91216e74ac6df5d94ca5fdf6
Shimberly <kimmy_11_07@hotmail.com>
2018-09-20 13:42:23 -0500

    Info app configuraciones

---
+++ appinfo/autoload.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * ownCloud - blendmaxmayapluginGUI
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Kimberly Muñoz <kipamuno@espol.edu.ec>
+ * @author Jean Paul Valle <jpvalle@espol.edu.ec>
+ * @copyright Edcom-Espol 2018
+ */
+
+namespace OCA\BlendMaxMayaPluginGui\AppInfo;
+
+use OCP\AppFramework\App;
+
+/**
+ * Additional autoloader registration, e.g. registering composer autoloaders
+ */
+// require_once __DIR__ . '/../vendor/autoload.php';
